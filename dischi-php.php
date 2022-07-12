da4c3158f35ca7a5749781a38f7b1d6b698b7c8d
silviawolves <silviacanepa.w@gmail.com>
2022-07-12 14:48:56 +0200

    milestone1 - completa

--- dischi-php.php
+++ dischi-php.php
@@ -10,14 +10,11 @@ include __DIR__ . '/db/db-dischi.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dischi</title>
+    <title>Dischi PHP</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
-    <!-- VUE -->
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 </head>
 
 <body>
@@ -56,8 +53,5 @@ include __DIR__ . '/db/db-dischi.php';
         </main>
 
     </div>
-    <!-- JS -->
-    <script src="script.js"></script>
-
 </body>
 </html>
