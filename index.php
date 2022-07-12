84223ce297e80f55909cc33b96c3698e01994a13
silviawolves <silviacanepa.w@gmail.com>
2022-07-12 13:41:49 +0200

    base codice

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- VUE -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+</head>
+
+<body>
+    <div id="app">
+
+        <header>
+            <img src="img/logo-small.svg" alt="logo spotify">
+        </header>
+
+
+        <main>
+            <div class="container py-5">
+
+                <div class="row">
+                    <div class="col">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </main>
+    </div>
+    <!-- JS -->
+    <script src="script.js"></script>
+
+</body>
+</html>
